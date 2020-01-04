fe00c3327c68732adb67a9466825425f1570b041
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-01-04 12:45:43 +0700

    Fix deployer

--- blog/deployer.py
+++ blog/deployer.py
@@ -4,19 +4,26 @@ sudo chmod go-rwx deployer.py && sudo chmod g+x deployer.py
 sudo visudo
 add to the end: "nobody ALL = NOPASSWD: deployer.py"
 """
+import logging
 import os
 from time import time
+
+
+logging.basicConfig(filename="flask.log", level=logging.DEBUG)
+logging.info('Запустили деплоер')
 
 
 def main():
     commands = ('cd ~/www/blog', 'git pull origin master', 'supervisorctl restart blog')
     for command in commands:
         try:
+            logging.info(f'Выполняем {command}')
             os.system(command)
+            logging.info(f'Выполнено: {command}')
         except Exception as e:
-            with open('update.log', 'a') as f:
-                f.write(f'{time()} --- Error during execution the command: {command} \n {e}')
+            logging.error(f'Не удалось выполнить {command}: {e}')
             break
+    return 'Success!', 200
 
 
 if __name__ == '__main__':
--- blog/view.py
+++ blog/view.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # https://github.com/KazakovDenis
+import logging
 import os
 import sys
 
@@ -8,16 +9,18 @@ from flask import render_template, request, url_for, redirect
 from models import Post, Tag
 
 
+logging.basicConfig(filename="flask.log", level=logging.DEBUG)
+
+
 @app.route('/hook', methods=['POST', 'GET'])
 def hook():
     if request.method == 'POST':
+        logging.info('Обработали пост-запрос')
         conditions = (request.headers.get('X-Hub-Signature'),)
         # request.data['repository']['id'] == 208764257, request.data['sender']['id'] == 45169520
         if all(conditions):
+            logging.info('Все условия соблюдены, приступаем к исполнению deployer')
             os.execl(sys.executable, 'python', 'deployer.py')
-            return 'Successfully', 200
-        else:
-            return 'Forbidden', 403
     return redirect(url_for('index'))
 
 
