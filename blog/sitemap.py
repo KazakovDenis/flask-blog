f20ef93b0d1ebcb4509ff3f707810922d8be0538
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-02-08 16:39:51 +0700

    Sitemap: regexp, docs, outer config

--- blog/sitemap.py
+++ blog/sitemap.py
@@ -3,10 +3,15 @@ from models import Post, Tag
 from sitemap_ext import Sitemap
 
 
-sitemap = Sitemap(app, 'https://kazakov.ru.net')
-sitemap.config.IGNORED.extend(['/logout', '/upload'])
-sitemap.config.FOLDER = ('static',)
-sitemap.add_rule('/blog', Post, updated='created')
+class Config:
+
+    FOLDER = ('static',)
+    IGNORED = ['/static', '/admin', ]
+    INDEX_PRIORITY = 1.0
+
+
+sitemap = Sitemap(app, 'https://kazakov.ru.net', config_obj=Config)
+sitemap.add_rule('/blog', Post, lastmod='created')
 sitemap.add_rule('/blog/tag', Tag, priority=0.8)
 
 
