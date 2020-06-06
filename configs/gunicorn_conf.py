43f5698e3665941a1db9a941f8dc07c472eb2f72
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-06 11:47:20 +0700

    gunicorn conf

--- configs/gunicorn_conf.py
+++ configs/gunicorn_conf.py
@@ -1,10 +1,15 @@
 import multiprocessing
+from os.path import join
+from sys import platform
+
+from app.config import PATH
 
 
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 600
 
-pythonpath = '/usr/bin/python3'
-accesslog = '/blog/log/gunicorn/access.log'
-errorlog = '/blog/log/gunicorn/error.log'
+if platform == 'linux':
+    pythonpath = '/usr/bin/python3'
+accesslog = join(PATH, 'log', 'gunicorn', 'access.log')
+errorlog = join(PATH, 'log', 'gunicorn', 'error.log')
