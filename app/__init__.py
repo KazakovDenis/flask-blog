01874bc216ae82f7e2fd9fa661bbd7d5577d9461
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-09 23:24:52 +0700

    removed run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
+"""
+The module to be pointed in WSGI configurations
+"""
 from .blog import *
+from . import view
+
+
+if __name__ == "__main__":
+    app.run()
--- app/run.py
+++
@@ -1,9 +0,0 @@
-"""
-The module to be pointed in WSGI configurations
-"""
-from .blog import app, db
-from . import view
-
-
-if __name__ == "__main__":
-    app.run()
