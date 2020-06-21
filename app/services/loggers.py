09975856732b8bd81e36c32839ca323f99a351ea
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-06-21 18:14:59 +0700

    log dirs fix

--- app/services/loggers.py
+++ app/services/loggers.py
@@ -1,13 +1,19 @@
 import logging
-from os.path import join
 
 from app.blog import app
 from app.config import *
 
+
+LOG_DIR = os.path.join(PATH, 'log')
+for name in ('flask', 'gunicorn'):
+    folder = os.path.join(LOG_DIR, name)
+    os.makedirs(folder, exist_ok=True)
+
+
 # app
 log = app.logger
 log.setLevel(LOG_LEVEL)
-fh = logging.FileHandler(join(PATH, 'log', 'flask', 'app.log'), encoding='utf-8')
+fh = logging.FileHandler(os.path.join(LOG_DIR, 'flask', 'app.log'), encoding='utf-8')
 fh.setLevel(LOG_LEVEL)
 fh.setFormatter(logging.Formatter(LOG_FORMAT))
 log.addHandler(fh)
@@ -15,7 +21,7 @@ log.addHandler(fh)
 # sitemap
 sm_log = logging.getLogger('sitemap')
 sm_log.setLevel(LOG_LEVEL)
-smh = logging.FileHandler(join(PATH, 'log', 'flask', 'sitemap.log'), encoding='utf-8')
+smh = logging.FileHandler(os.path.join(LOG_DIR, 'flask', 'sitemap.log'), encoding='utf-8')
 smh.setLevel(LOG_LEVEL)
 smh.setFormatter(logging.Formatter(LOG_FORMAT))
 sm_log.addHandler(fh)
