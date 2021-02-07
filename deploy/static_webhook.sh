7563dbde97060dc8e65b15967440ee9b285c7c07
Denis Kazakov <kazakovdeyu@yandex.ru>
2021-02-07 23:11:58 +0300

    fix nginx conf

--- deploy/static_webhook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# The script to update static files from Github
-set -e
-echo ">>>>> Pulling static files & templates..."
-
-shopt -s expand_aliases
-source /etc/environment
-source /etc/profile.d/aliases.sh
-pullstatic
